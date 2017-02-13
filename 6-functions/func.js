41ac949b37efd40dee3a9941dc8109cb0532149c
Melissa E Hare <hare.melissa@gmail.com>
2017-02-13 13:56:04 -0600

    Update func.js

--- 6-functions/func.js
+++ 6-functions/func.js
@@ -1,13 +1,21 @@
 // 1 - Write a function declaration
 // that takes no arguments and
 // returns the value 4
-
+function myFunction() {
+  return 4;
+}
 // 2 - Write a function declaration
 // that takes two arguments and
 // returns the value of the result
 // of the multiplication of both
 // arguments
-
+function thisFunction(4, 8) {
+  var product = 4 * 8;
+  return product;
+}
 // 3 - Write a function expression
 // that takes one argument and
 // console logs that value.
+var someFunction = function(16) {
+  console.log(someFunction);
+}
